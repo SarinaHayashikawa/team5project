1c355a7749b8993f691b57048d9618e8c14f0b77
yosidayuto <yuu-poi-921@i.softbank.jp>
2021-06-10 14:55:39 +0900

    カメラをデバック用に自由に動くように

--- Teamproject/Teamproject/camera.cpp
+++ Teamproject/Teamproject/camera.cpp
@@ -25,8 +25,9 @@
 #define CAMERA_INTERPOLATION 0.2f //カメラ補間スピード
 
 #ifdef _DEBUG
-#define CAMERA_SENSITIVITY (5)	//マウス感度
-#define CAMERA_SPEED (10.0f)	//カメラスピード
+#define CAMERA_SENSITIVITY (5)	// マウス感度
+#define CAMERA_SPEED (10.0f)	// カメラスピード
+#define CAMERA_ZOOM	(50.0f)		// カメラズーム量
 #endif
 
 //======================================================
@@ -45,8 +46,12 @@ CCamera::CCamera()
 	m_posVDest = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 	m_posRDest = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 	m_fInterpolation = 0.0f;
-	
+
+#ifdef _DEBUG
 	m_fMoveRot = 0.0f;
+	m_pos = m_posV;
+#endif
+
 }
 //======================================================
 //デストラクタ
@@ -62,7 +67,7 @@ HRESULT CCamera::Init(void)
 {
 	//カメラの初期(位置・注意点・上方向)設定
 	m_posV = D3DXVECTOR3(20, 100, -200);
-	m_posR = D3DXVECTOR3(0, 0, 500);
+	m_posR = m_posV + D3DXVECTOR3(0, 0, 500);
 	m_vecU = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
 	//カメラの向き設定
 	m_rot = D3DXVECTOR3(45.0f, 180.0f, 0.0f);
@@ -90,15 +95,29 @@ void CCamera::Update(void)
 {
 #ifdef _DEBUG
 	Move();
+	//移動の計算
+	D3DXVECTOR3 RotateCenter = m_pos + D3DXVECTOR3(sinf(D3DXToRadian(m_rot.y - 90)),
+		50,
+		cosf(D3DXToRadian(-m_rot.y - 90)));
 
-#endif
+	//カメラの移動処理
+	m_posV = RotateCenter +
+		D3DXVECTOR3(sinf(D3DXToRadian(-m_rot.y)) * cosf(D3DXToRadian(m_rot.x)) * 1,
+			sinf(D3DXToRadian(m_rot.x)) * 1,
+			cosf(D3DXToRadian(-m_rot.y)) * cosf(D3DXToRadian(m_rot.x)) * 1);
 
-	//CPlayer* pPlayer = CManager::GetPlayer();
+	//注意点の移動処理
+	m_posR = RotateCenter;
+
+#else
+	CPlayer* pPlayer = CManager::GetPlayer();
 
 	//カメラ距離の変化を補完
 	m_posV += (m_posVDest - m_posV) * m_fInterpolation;
 	//カメラ距離の変化を補完
 	m_posR += (m_posRDest - m_posR) * m_fInterpolation;
+
+#endif
 
 }
 
@@ -166,34 +185,56 @@ void CCamera::SetCameraView(D3DXVECTOR3 posV, D3DXVECTOR3 posR, float fInterpola
 //=============================================================================
 void CCamera::Move(void)
 {
-
 	//キーボード入力の取得
 	CKeyboard* pInput = (CKeyboard*)CManager::GetInputKeyboard();
-
 	//マウス入力取得
 	CMouse *pInputMouse = (CMouse*)CManager::GetInputMouse();
 	//移動量
 	D3DXVECTOR3 move = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	//注視点までの距離
+	D3DXVECTOR3 distance = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+
 	//視点を動かす
 	if (pInputMouse->GetClick(1))
 	{
 		//向き
 		m_rot.x += (pInputMouse->GetMousePos().lY * (float)(0.01f * CAMERA_SENSITIVITY));
 		m_rot.y -= (pInputMouse->GetMousePos().lX * (float)(0.01f * CAMERA_SENSITIVITY));
+		
+		//注視点の距離を変える
+		if (pInputMouse->GetMousePos().lZ != 0)
+		{
+			//単位ベクトルに取得
+			D3DXVec3Normalize(&distance, &(m_posR - m_posV));
+
+			//ズームアウトの場合
+			if (pInputMouse->GetMousePos().lZ<0)
+			{
+				distance *= -1;
+			}
+		}
 
 		//移動
-		if (pInput->GetKeyPress(DIK_UP))
+		if (pInput->GetKeyPress(DIK_W))
 		{
-			//単位ベクトルに取得
-			D3DXVec3Normalize(&move, &(m_posR - m_posV));
+			//移動方向指定
+			m_fMoveRot = -90.0f;
+			//移動時の向き設定
+			m_fMoveRot += m_rot.y;
+			//移動量処理
+			move += D3DXVECTOR3(cosf(D3DXToRadian(m_fMoveRot)), 0, sinf(D3DXToRadian(m_fMoveRot)));
 		}
-		if (pInput->GetKeyPress(DIK_DOWN))
+		if (pInput->GetKeyPress(DIK_S))
 		{
-			//単位ベクトルに取得
-			D3DXVec3Normalize(&move, &(m_posR - m_posV));
-			move *= -1;
+			//移動方向指定
+			m_fMoveRot = 90.0f;
+			//移動時の向き設定
+			m_fMoveRot += m_rot.y;
+			//移動量処理
+			move += D3DXVECTOR3(cosf(D3DXToRadian(m_fMoveRot)), 0, sinf(D3DXToRadian(m_fMoveRot)));
 		}
-		if (pInput->GetKeyPress(DIK_LEFT))
+
+		if (pInput->GetKeyPress(DIK_A))
 		{
 			//移動方向指定
 			m_fMoveRot = 0.0f;
@@ -202,7 +243,7 @@ void CCamera::Move(void)
 			//移動量処理
 			move += D3DXVECTOR3(cosf(D3DXToRadian(m_fMoveRot)), 0, sinf(D3DXToRadian(m_fMoveRot)));
 		}
-		if (pInput->GetKeyPress(DIK_RIGHT))
+		if (pInput->GetKeyPress(DIK_D))
 		{
 			//移動方向指定
 			m_fMoveRot = 180.0f;
@@ -211,12 +252,18 @@ void CCamera::Move(void)
 			//移動量処理
 			move += D3DXVECTOR3(cosf(D3DXToRadian(m_fMoveRot)), 0, sinf(D3DXToRadian(m_fMoveRot)));
 		}
+
 		//速度処理
 		move *= CAMERA_SPEED;
-		//移動処理
-		m_posV += move;
-	}
+		//ズームの感度
+		distance *= CAMERA_ZOOM;
 
+		//移動処理
+		m_pos += move;
+		//ズーム処理
+		m_pos += distance;
+
+	}
 
 	//向きの限界値処理
 	if ((m_rot.x) >= 89
@@ -232,6 +279,7 @@ void CCamera::Move(void)
 	{
 		m_rot.y -= 360;
 	}
+
 
 
 }
--- Teamproject/Teamproject/camera.h
+++ Teamproject/Teamproject/camera.h
@@ -40,22 +40,26 @@ private:
 
 	void SetCameraView(D3DXVECTOR3 posV, D3DXVECTOR3 posR, float fInterpolation);
 
-	D3DXVECTOR3 m_posV;            //カメラの座標
-	D3DXVECTOR3 m_posR;            //注視点の座標
-	D3DXVECTOR3 m_vecU;            //カメラの上方向
-	D3DXMATRIX m_mtxProjection;    //プロジェクションマトリックス
-	D3DXMATRIX m_mtxView;          //ビューマトリックス
-	D3DXVECTOR3 m_RotateCenter;    //カメラの回転中心座標
-	float m_fDistance;             //視点-注意点の距離
+	D3DXVECTOR3 m_posV;            // カメラの座標
+	D3DXVECTOR3 m_posR;            // 注視点の座標
+	D3DXVECTOR3 m_vecU;            // カメラの上方向
+	D3DXMATRIX m_mtxProjection;    // プロジェクションマトリックス
+	D3DXMATRIX m_mtxView;          // ビューマトリックス
+	D3DXVECTOR3 m_RotateCenter;    // カメラの回転中心座標
+	float m_fDistance;             // 視点-注意点の距離
 	float m_fDistanceFromPlayer;
 	float m_fDistanceFromPlayerDest;
-	D3DXVECTOR3 m_rot;
-	D3DXVECTOR3 m_posVDest;         //カメラの座標(差)
-	D3DXVECTOR3 m_posRDest;         //注視点の座標(差)
-	bool m_bIsFollowPlayer;			//プレイヤーに追従するか
-	float m_fInterpolation;			//カメラ補間スピード
-	
-	float m_fMoveRot;				//移動する向き
+	D3DXVECTOR3 m_rot;				   
+	D3DXVECTOR3 m_posVDest;         // カメラの座標(差)
+	D3DXVECTOR3 m_posRDest;         // 注視点の座標(差)
+	bool m_bIsFollowPlayer;			// プレイヤーに追従するか
+	float m_fInterpolation;			// カメラ補間スピード
+
+#ifdef _DEBUG						   
+	float m_fMoveRot;				// 移動する向き
+	D3DXVECTOR3 m_pos;				// 位置
+#endif
+
 };
 
 #endif
