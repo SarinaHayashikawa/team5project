fa913c88d05d1de528b91b87bf6c12c5fc869dd3
SarinaHayashikawa <191019@st.yoshida-g.ac.jp>
2021-07-29 09:07:50 +0900

    backup

--- Teamproject/Teamproject/game.cpp
+++ Teamproject/Teamproject/game.cpp
@@ -29,6 +29,7 @@
 #include "map.h"
 #include "Shield.h"
 #include "scoreup.h"
+#include "Fieldmanager.h"
 //=============================================================================
 // 静的メンバ変数初期化
 //=============================================================================
@@ -76,9 +77,8 @@ HRESULT CGame::Init()
 	//オブジェクト生成
 	CManager::CreateCamera();
 	CManager::CreateLight();
-
-	//ステージ生成
-	CFloor::Create(D3DXVECTOR3(0.0f, -50.0f, 0.0f), D3DXVECTOR3(500.0f, 500.0f, 500.0f));
+	//ここで床生成
+	CFieldManager::Create(D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DXVECTOR3(500.0f, 0.0f, 500.0f));
 	
 	//デバックのためのアイテム
 	CEbi::Create(D3DXVECTOR3(30.0f, 0.0f, 0.0f), D3DXVECTOR3(10.0f, 10.0f, 0.0f));
